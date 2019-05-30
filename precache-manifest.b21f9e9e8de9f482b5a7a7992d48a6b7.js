f4a29c86d262c4f11a365c6f5d4330df0ddb1526
Pawel Butra <pawel.butra@onet.pl>
2019-05-30 14:30:44 +0200

    Updates

---
+++ precache-manifest.b21f9e9e8de9f482b5a7a7992d48a6b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "94cf1695c205b80ab813",
+    "url": "/static/js/main.b8899228.chunk.js"
+  },
+  {
+    "revision": "3db8510cffffcac10601",
+    "url": "/static/js/2.659f3cde.chunk.js"
+  },
+  {
+    "revision": "94cf1695c205b80ab813",
+    "url": "/static/css/main.213941bd.chunk.css"
+  },
+  {
+    "revision": "9cd3f954fa661142ea466e819b3168b5",
+    "url": "/index.html"
+  }
+];
